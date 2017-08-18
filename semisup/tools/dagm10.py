af5557a58bb1be6659044d533651b0666ab3e5ec
hongdoki <cfhd89@gmail.com>
2017-08-18 16:38:59 +0900

    add dagm dataset tools

---
+++ semisup/tools/dagm10.py
@@ -0,0 +1,25 @@
+from __future__ import division
+from __future__ import print_function
+
+import numpy as np
+import data_dirs
+import os
+
+DATADIR = data_dirs.dagm
+
+
+NUM_LABELS = 2
+IMAGE_SHAPE = [256, 256, 1]
+
+
+def get_data(name):
+  """Utility for convenient data loading."""
+
+  if name == 'train' or name == 'unlabeled':
+    return np.expand_dims(np.load(os.path.join(DATADIR, 'source_d10_X_train.npy')), axis=-1), \
+           np.argmax(np.load(os.path.join(DATADIR, 'source_d10_y_train.npy')), axis=1)
+  # custom svhn has only training/validation set
+  elif name == 'test':
+    return np.expand_dims(np.load(os.path.join(DATADIR, 'source_d10_X_val.npy')), axis=-1), \
+           np.argmax(np.load(os.path.join(DATADIR, 'source_d10_y_val.npy')), axis=1)
+
