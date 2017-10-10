a37ca708212976576b31cc75780d13356b2e9981
hongdoki <cfhd89@gmail.com>
2017-10-10 17:27:09 +0900

    slightly fixed

--- semisup/tools/svhn_custom.py
+++ semisup/tools/svhn_custom.py
@@ -11,7 +11,7 @@ DATADIR = data_dirs.svhn_custom
 NUM_LABELS = 10
 IMAGE_SHAPE = [32, 32, 3]
 
-SPLITS_TO_SIZES = {'train': 54000, 'val': 6000, 'test': 10000}
+SPLITS_TO_SIZES = {'train': 65932, 'val': 7325}
 FILE_PATTERN = 'source_svhn_%s.tfrecords'
 ITEMS_TO_DESCRIPTIONS = {
     'image': 'A [32 x 32 x 3] RGB image.',
@@ -25,6 +25,6 @@ def get_data(name):
     return np.load(os.path.join(DATADIR, 'source_svhn_train_X.npy')), \
            np.argmax(np.load(os.path.join(DATADIR, 'source_svhn_train_y.npy')), axis=1)
   # custom svhn has only training/validation set
-  elif name == 'test':
+  elif name == 'test' or name == 'validation':
     return np.load(os.path.join(DATADIR, 'source_svhn_val_X.npy')), \
            np.argmax(np.load(os.path.join(DATADIR, 'source_svhn_val_y.npy')), axis=1)
